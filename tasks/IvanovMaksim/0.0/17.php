af6e872f9dd0f2bbe2b584fed6aa3b896b44d00f
LiiChar <yura22875@yandex.ru>
2023-01-21 17:46:58 +0500

    introductionOnPHP

---
+++ tasks/IvanovMaksim/0.0/17.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $vera = 'Максим любит банановое молоко';
+	print(mb_strlen($vera)); 
+?>
+</body>
+</html>
---
+++ tasks/IvanovMaksim/0.0/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    function sum($a, $b) {
+        return $a + $b;
+    }
+    function multiplay($a, $b) {
+        return $a * $b;
+    }
+    $arr = array[];
+
+    echo(sum(4234234, 10000000));  // выведет сумму
+    echo(multiplay(1234, 23424)); // xnj dsqltn
+    ?>
+  
+</body>
+</html>
